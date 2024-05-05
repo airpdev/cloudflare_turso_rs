529c785a435dc6c73cc4984ace2be7903f5f1fbd
Petar Vasilev <petar.vasilev0727@gmail.com>
2024-05-05 14:27:05 +0400

    initial step

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use worker::*;
+// use libsql::Builder;
+use std::env;
+
+#[event(fetch)]
+async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
+    let url = env::var("TURSO_DATABASE_URL").expect("LIBSQL_URL must be set");
+    let token = env::var("TURSO_AUTH_TOKEN").unwrap_or_default();
+
+    // let db = Builder::new_remote(&url, &token).build().await?;
+    // let conn = db.connect().unwrap();
+    // conn.execute("SELECT * FROM members", ()).await?;
+
+    Response::ok("Hello, World!")
+
+}
