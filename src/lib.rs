0f5a8611dd86c26f059e91c72bcbd94883379649
Petar Vasilev <petar.vasilev0727@gmail.com>
2024-05-05 16:37:38 +0400

    added codes to connect Turso db

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use worker::*;
-// use libsql::Builder;
+use libsql::Builder;
 use std::env;
 
 #[event(fetch)]
@@ -7,9 +7,9 @@ async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
     let url = env::var("TURSO_DATABASE_URL").expect("LIBSQL_URL must be set");
     let token = env::var("TURSO_AUTH_TOKEN").unwrap_or_default();
 
-    // let db = Builder::new_remote(&url, &token).build().await?;
-    // let conn = db.connect().unwrap();
-    // conn.execute("SELECT * FROM members", ()).await?;
+    let db = Builder::new_remote(&url, &token).build().await.unwrap();
+    let conn = db.connect().unwrap();
+    conn.execute("SELECT * FROM members", ()).await?;
 
     Response::ok("Hello, World!")
 }
